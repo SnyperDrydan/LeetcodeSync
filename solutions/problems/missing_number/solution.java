186d099f7f363d5dae1fdf236792736f2057b99d
Snyper Drydan <anujjadon98@gmail.com>
2023-10-08 08:59:12 +0000

    Sync LeetCode submission - Missing Number (java)

---
+++ solutions/problems/missing_number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0, xor2 = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            xor1 ^= nums[i];
+            xor2 ^= i;
+        }
+
+        xor2 ^= nums.length;
+
+        return xor1^xor2;
+    }
+}

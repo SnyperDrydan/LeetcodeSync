eda4a941b0583b9e944d2cb058ff99e284e9475c
Snyper Drydan <anujjadon98@gmail.com>
2023-10-08 13:03:49 +0000

    Sync LeetCode submission - Majority Element (java)

--- solutions/problems/majority_element/solution.java
+++ solutions/problems/majority_element/solution.java
@@ -2,14 +2,17 @@ class Solution {
     public int majorityElement(int[] nums) {
         int count = 0, candidate = nums[0];
 
-        for(int num: nums) {
-            if(count == 0)
-                candidate = num;
-            if(num == candidate)
+        for(int i = 0; i < nums.length; i++) {
+            if(count == 0) {
+                candidate = nums[i];
+            }
+
+            if(nums[i] == candidate)
                 count++;
             else
                 count--;
         }
+
         return candidate;
     }
-}   
+}

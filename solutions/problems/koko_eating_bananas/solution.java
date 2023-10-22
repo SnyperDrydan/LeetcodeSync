a99937cc942c32aa952e98b7b07225d7b452b8ab
Snyper Drydan <anujjadon98@gmail.com>
2023-10-22 15:21:17 +0000

    Sync LeetCode submission - Koko Eating Bananas (java)

--- solutions/problems/koko_eating_bananas/solution.java
+++ solutions/problems/koko_eating_bananas/solution.java
@@ -1,22 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int l = 1, r = 1;
-        for(int i: piles)
-            r = Math.max(r, i);
-        int res = r;
+        int min = 1;
+        int max = Integer.MIN_VALUE;
 
-        while(l<r) {
-            int m = (l+r)/2;
-            int hours = 0;
-            for(int i : piles)
-                hours += Math.ceil((double) i/m);
-            if(hours<=h) {
-                r = m;
-            } else {
-                l = m+1;
-            }
-
+        for(int a : piles) {
+            max = Math.max(max,a);
         }
-        return r;
+
+        while(min < max) {
+            int mid = (min+max)/2;
+            if(getHoursToEat(piles, mid) > h) {
+                min = mid + 1;
+            } else {
+                max = mid;
+            }
+        }
+
+        return min;
+    }
+
+    private int getHoursToEat(int[] piles, int k) {
+        int h = 0;
+        for(int a : piles) {
+            h += Math.ceil((double)a/k);
+        }
+        return h;
     }
 }

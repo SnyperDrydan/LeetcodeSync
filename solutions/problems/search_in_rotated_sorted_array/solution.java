498e22f6a5b2ba18b3ccc3279225cb186235239c
Snyper Drydan <anujjadon98@gmail.com>
2023-10-28 15:19:57 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- solutions/problems/search_in_rotated_sorted_array/solution.java
+++ solutions/problems/search_in_rotated_sorted_array/solution.java
@@ -1,6 +1,5 @@
 class Solution {
     public int search(int[] nums, int target) {
-
         int l = 0, h = nums.length-1;
 
         while(l<=h) {
@@ -8,25 +7,22 @@ class Solution {
 
             if(nums[m] == target)
                 return m;
-
-            if(nums[l] <= nums[m]) {
+            
+            if(nums[l]<=nums[m]) {
                 if(nums[l] <= target && target <= nums[m]) {
                     h = m-1;
-                }
-                else {
+                } else {
                     l = m+1;
                 }
             } else {
                 if(nums[m] <= target && target <= nums[h]) {
                     l = m+1;
-                }
-                else {
+                } else {
                     h = m-1;
                 }
             }
         }
 
         return -1;
-        
     }
 }

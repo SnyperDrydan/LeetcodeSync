21705bffd811200888bedeba8064a65bf2420140
Snyper Drydan <anujjadon98@gmail.com>
2023-11-14 15:18:34 +0000

    Sync LeetCode submission - Remove Element (java)

--- solutions/problems/remove_element/solution.java
+++ solutions/problems/remove_element/solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int x = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != val) {
-                nums[x] = nums[i];
-                x++;
+        int i = 0;
+        int j = 0;
+        while(j < nums.length) {
+            if(nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
+            j++;
         }
-        return x;
+
+        return i;
     }
 }

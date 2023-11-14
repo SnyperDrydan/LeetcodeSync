042e86cbcd177ea8fcd3324aae11bed10e0bbb30
Snyper Drydan <anujjadon98@gmail.com>
2023-11-14 03:22:56 +0000

    Sync LeetCode submission - Subsets (java)

--- solutions/problems/subsets/solution.java
+++ solutions/problems/subsets/solution.java
@@ -3,18 +3,19 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> set = new ArrayList<>();
         dfs(res, set, nums, 0);
-        return res; 
+
+        return res;
     }
 
-    public void dfs(List<List<Integer>> res, List<Integer> set, int[] nums, int start) {
-        if(start >= nums.length) {
+    public void dfs(List<List<Integer>> res, List<Integer> set, int[] nums, int cur) {
+        if(cur >= nums.length) {
             res.add(new ArrayList<>(set));
             return;
         }
 
-        set.add(nums[start]);
-        dfs(res, set, nums, start+1);
+        set.add(nums[cur]);
+        dfs(res, set, nums, cur+1);
         set.remove(set.size() - 1);
-        dfs(res, set, nums, start+1);
+        dfs(res, set, nums, cur+1);
     }
 }

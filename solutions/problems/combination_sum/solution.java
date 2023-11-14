d8649d4a147e7fd0d81f95efdf76c3b419f44689
Snyper Drydan <anujjadon98@gmail.com>
2023-11-14 04:04:42 +0000

    Sync LeetCode submission - Combination Sum (java)

--- solutions/problems/combination_sum/solution.java
+++ solutions/problems/combination_sum/solution.java
@@ -2,28 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> set = new ArrayList<>();
-        Arrays.sort(candidates);
         dfs(res, set, candidates, target, 0);
+
         return res;
     }
 
-    public void dfs(List<List<Integer>> res, List<Integer> set, int[] nums, int remaining, int start) {
-        if(remaining < 0) {
-            return;
-        }
-
-        if(start >= nums.length) {
-            return;
-        }
-
-        if(remaining == 0) {
+    public void dfs(List<List<Integer>> res, List<Integer> set, int[] candidates, int target, int val) {
+        if(target == 0)
+        {
             res.add(new ArrayList<>(set));
             return;
-        }
-
-        set.add(nums[start]);
-        dfs(res, set, nums, remaining - nums[start], start);
+        } else if(target < 0 || val >= candidates.length)
+            return;
+        
+        set.add(candidates[val]);
+        dfs(res, set, candidates, target - candidates[val], val);
         set.remove(set.size() - 1);
-        dfs(res, set, nums, remaining, start+1);
+        dfs(res, set, candidates, target, val+1);
+
     }
 }

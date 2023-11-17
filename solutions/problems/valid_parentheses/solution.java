ff8f728c861c50b33bb332cd4fd5a6c02fa7bbea
Snyper Drydan <anujjadon98@gmail.com>
2023-11-17 15:56:35 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- solutions/problems/valid_parentheses/solution.java
+++ solutions/problems/valid_parentheses/solution.java
@@ -1,37 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-
-        if(s == null || s.isEmpty())
-            return true;
-
         Stack<Character> stack = new Stack<>();
 
-        List<Character> opening = Arrays.asList('(', '{', '[');
-        List<Character> closing = Arrays.asList(')', '}', ']');
-        
-        for(int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            if(opening.contains(c)) {
-                stack.push(c);
-                System.out.println(stack);
-            }
-            else {
-                if (stack.empty()) {
+        for(char ch: s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if(stack.size() == 0)
                     return false;
-                }
-                if (c == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
-                    stack.pop();
-                } else {
+                if(ch == ')' && stack.pop() != '(')
                     return false;
-                }
+                else if(ch == '}' && stack.pop() != '{')
+                    return false;
+                else if(ch == ']' && stack.pop() != '[')
+                    return false;
             }
-        } 
-        if(stack.size() > 0)
-        return false;
-        return true;
+        }
+
+        return stack.size() > 0 ? false : true;
     }
 }

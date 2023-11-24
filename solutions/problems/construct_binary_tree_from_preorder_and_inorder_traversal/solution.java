56935c27608a1aa939d4c355c02de3763e995776
Snyper Drydan <anujjadon98@gmail.com>
2023-11-24 17:25:26 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (java)

--- solutions/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.java
+++ solutions/problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.java
@@ -15,22 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
+        return buildTree(preorder, 0, preorder.length -1, inorder, 0, inorder.length -1);
     }
 
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) {
+    public TreeNode buildTree(int[] preorder, int startPre, int endPre, int[] inorder, int startIn, int endIn) {
+        if (startPre > endPre || startIn > endIn) {
             return null;
         }
-        TreeNode root = new TreeNode(preorder[preStart]);
-        int inIndex = 0; // Index of current root in inorder
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == root.val) {
-                inIndex = i;
-            }
+        TreeNode node = new TreeNode(preorder[startPre]);
+        int rootIndex = startIn;
+        while (inorder[rootIndex] != preorder[startPre]) {
+            rootIndex++;
         }
-        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
-        return root;
+
+        int leftSubtreeSize = rootIndex - startIn;
+
+        node.left = buildTree(preorder, startPre + 1, startPre + leftSubtreeSize, inorder, startIn, rootIndex - 1);
+        node.right = buildTree(preorder, startPre + leftSubtreeSize + 1, endPre, inorder, rootIndex + 1, endIn);
+        return node;
     }
 }

382710eb7010b46e40c96279ee39c7dfda37bbe5
Snyper Drydan <anujjadon98@gmail.com>
2023-12-30 04:47:36 +0000

    Sync LeetCode submission - Set Matrix Zeroes (java)

--- solutions/problems/set_matrix_zeroes/solution.java
+++ solutions/problems/set_matrix_zeroes/solution.java
@@ -1,37 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-
-        int firstRow = 1;
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
 
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j] == 0) {
-                    if(i == 0)
-                        firstRow = 0;
-                    else {
-                        matrix[i][0] = 0;
-                    }
-
-                    matrix[0][j] = 0;
-                    
+                    row[i] = 1;
+                    col[j] = 1;
                 }
-            }
-        }
-
-        for(int i = matrix.length-1; i >= 0; i--) {
-            for(int j = matrix[0].length-1; j >= 0; j--) {
-                if(i == 0) {
-                    if(firstRow == 0 || matrix[0][j] == 0) {
-                        matrix[i][j] = 0;
-                    }
-                }
-                else {
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                        matrix[i][j] = 0;
-                    }
-                }
-            }
+            }        
         }
         
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(row[i] == 1 || col[j] == 1)
+                    matrix[i][j] = 0;
+            }
+        }
     }
 }

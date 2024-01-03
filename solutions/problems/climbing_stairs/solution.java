8e55764817b16346c22781ea637fe1371db953cf
Snyper Drydan <anujjadon98@gmail.com>
2024-01-03 12:07:41 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- solutions/problems/climbing_stairs/solution.java
+++ solutions/problems/climbing_stairs/solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] arr = new int[n+1];
-        return traverse(n , arr);
-    }
+        int prev2 = 1;
+        int prev1 = 1;
+        for(int i = 2; i < n+1; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
 
-    public int traverse(int n, int[] arr) {
-        if(n <= 1)
-            return 1;
-        if(arr[n] != 0) {
-            return arr[n];
-        }
-        else {
-            arr[n] = traverse(n-1, arr) + traverse(n-2, arr);
-            return arr[n];
-        }
+        return prev2;
     }
 }
